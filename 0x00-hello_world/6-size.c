7f413a5e8f91db40602f7566559e705e32e4dcfd
Stephen Tawiah <stawiah17@gmail.com>
2022-09-08 15:57:38 -0700

    removed i from printif()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printif("Size of a char: %d bytes \n", sizeof(char));
-	printif("Size of a int: %d bytes \n", sizeof(int));
-	printif("Size of a long int: %d bytes \n", sizeof(long int));
-	printif("Size of a long long int: %d bytes \n", sizeof(long long int));
-	printif("Size of a float: %d bytes \n", sizeof(float));
+	printf("Size of a char: %d bytes \n", sizeof(char));
+	printf("Size of a int: %d bytes \n", sizeof(int));
+	printf("Size of a long int: %d bytes \n", sizeof(long int));
+	printf("Size of a long long int: %d bytes \n", sizeof(long long int));
+	printf("Size of a float: %d bytes \n", sizeof(float));
 			return (0);
 }
