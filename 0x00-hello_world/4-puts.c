f308d3b9e0df68f48598b2ae00fb7343ed472816
Stephen Tawiah <stawiah17@gmail.com>
2022-09-08 12:58:11 -0700

    print with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
