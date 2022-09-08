563b9c2f125640072e91848199f966349db4d619
Stephen Tawiah <stawiah17@gmail.com>
2022-09-08 15:35:25 -0700

    size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printif("%lu\n", sizeof(char));
+	printif("%lu\n", sizeof(int));
+	printif("%lu\n", sizeof(long int));
+	printif("%lu\n", sizeof(long long int));
+	printif("%lu\n", sizeof(float);
+			return (0);
+}
