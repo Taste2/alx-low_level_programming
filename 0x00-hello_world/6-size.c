cf945bc0d49cae3a8a9e3dd43306afe020099b06
Stephen Tawiah <stawiah17@gmail.com>
2022-09-08 15:54:49 -0700

    Changed specifiers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printif("%lu\n", sizeof(char));
-	printif("%lu\n", sizeof(int));
-	printif("%lu\n", sizeof(long int));
-	printif("%lu\n", sizeof(long long int));
-	printif("%lu\n", sizeof(float));
+	printif("Size of a char: %d bytes \n", sizeof(char));
+	printif("Size of a int: %d bytes \n", sizeof(int));
+	printif("Size of a long int: %d bytes \n", sizeof(long int));
+	printif("Size of a long long int: %d bytes \n", sizeof(long long int));
+	printif("Size of a float: %d bytes \n", sizeof(float));
 			return (0);
 }
