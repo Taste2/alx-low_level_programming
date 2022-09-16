f17853ea4864a0b420bc3b11f6169e87ba2ab654
Stephen Tawiah <stawiah17@gmail.com>
2022-09-16 15:57:50 -0700

    upper case fxn created

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if is uppercase and 0 if it is not
+ */
+
+int _isupper(int c)
+{
+	if (if c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
