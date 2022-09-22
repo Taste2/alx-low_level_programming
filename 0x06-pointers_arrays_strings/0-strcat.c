eaf1773b2fdcfcf8a0fc04cfd012d7919417779d
Stephen Tawiah <stawiah17@gmail.com>
2022-09-22 15:45:14 -0700

    o task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: first string 
+ * @src: second string
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int u = 0;
+	int v = 0;
+	/*iterate until the last character is the null character*/
+	while (dest[u] != '\0')
+	{
+		u++;
+	}
+	/*replace the null character with the src string*/
+	while (src[v] != '\0')
+	{
+		dest[u] = src[v];
+		v++;
+		u++;
+	}
+	
+	dest[i] = '\0';
+	return (dest);
+
+}
+
