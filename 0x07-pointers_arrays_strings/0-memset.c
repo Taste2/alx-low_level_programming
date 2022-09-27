396aa46b12cfb5ca0ca08a11d879bf71166052d8
Stephen Tawiah <stawiah17@gmail.com>
2022-09-27 02:49:28 -0700

    memset edited

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - sets a character ata memory location
+ * @s: a poiter to a location
+ * @b: the character to set at a memory
+ * @n: the size or times to set the new character
+ * Return: The pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
