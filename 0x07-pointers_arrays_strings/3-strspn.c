bbe76c62655e42eedb10f412435eb024e95876d6
Stephen Tawiah <stawiah17@gmail.com>
2022-09-27 16:24:51 -0700

    strspn edited

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - searches char in a str
+ * @s: string to searched
+ * @accept: characters to find in s
+ * Return: number of characters found
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count_num = 0;
+
+	unsigned int i = 0;
+
+	unsigned int j;
+
+	while (accept[i] != '\0')
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				count_num++;
+			}
+		}
+		i++;
+	}
+	return (count_num);
+}
