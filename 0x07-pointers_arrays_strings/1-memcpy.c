c41f61b3d9553d115e6b88490c7bacf7917a14bf
Stephen Tawiah <stawiah17@gmail.com>
2022-09-27 05:50:35 -0700

    memcpy edited

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - copies a mem location to another
+ * @dest: destination
+ * @src: source
+ * @n: number of times to cpy
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int n;
+
+	for (i = 0, i < n, i++)
+	{
+		dest[i] = src[i]
+	}
+	return (dest);
+}
