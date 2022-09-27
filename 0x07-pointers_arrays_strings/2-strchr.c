ec6108b5d3551320ba3e866ecf2f1474b3df5ac6
Stephen Tawiah <stawiah17@gmail.com>
2022-09-27 08:13:17 -0700

    strchr edited

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strchr - find a char in a str
+ * @s: the string to be looked into
+ * @c: the char to be searched
+ * Return: Null if not found else c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int n = 0;
+
+	int i;
+
+	while (s[n] = '\0')
+	{
+		n++;
+		return (n);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (s[i] != c)
+		{
+			return (NULL);
+		}
+		else
+		{
+			return (c);
+		}
+	}
+}
