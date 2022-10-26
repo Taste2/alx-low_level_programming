32e64941eeefe0b7995d1ba46b15d493f5db52dd
Stephen Tawiah <stawiah17@gmail.com>
2022-10-26 04:47:56 -0700

    resolved betty errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,15 @@
 #include "list.h"
 
 /**
-* print_listing - prints the elements
+*print_listint - prints the elements
 *@h: listing list
-*Return - The number of nodes
+*Return:  The number of nodes
 */
 
 size_t print_listint(const listint_t *h)
 {
 	listint_t *trav;
-	
+
 	int count = 0;
 
 	trav = h;
