16ca827bcb1d53ef860921bd10d2b225fa6ee92c
Stephen Tawiah <stawiah17@gmail.com>
2022-10-26 15:36:04 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements
+ * @h: pointer to a node
+ * Return: The number of elemwnts
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h->n)
+	{
+		count++;
+	}
+	while (h->next)
+	{
+		count++;
+	}
+	return (count);
+}
