4012385e603b0296436ac70169450924a3ed9eae
Stephen Tawiah <stawiah17@gmail.com>
2022-10-26 16:33:31 -0700

    sizeof eprand amended

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	new = (listint_t *) malloc(sizeof(listint_int));
+	new = (listint_t *) malloc(sizeof(listint_t));
 	new->n = n;
 	new->next = head;
 	head = new;
