fe0e38cacbec22896a906a2426220c4d87ce5cc6
Stephen Tawiah <stawiah17@gmail.com>
2022-10-28 03:42:34 -0700

    0task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to unsigned
+ * int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if any of
+ * the chars in b is not 1 or 0. Or if b is NUL
+ * L
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len, i, pow = 1;
+
+	unsigned int decima_l = 0;
+
+	len = strlen(b);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b == NULL || b[i] != '0' || b[i] != '1')
+		{
+			return (0);
+		}
+		if (b[i] == '0')
+		{
+			int j = 0;
+		}
+		else if (b[i] == '1')
+		{
+			int j = 1;
+		}
+
+		decima_l += j * pow;
+		pow += pow;
+	}
+	return (decima_l);
+}
