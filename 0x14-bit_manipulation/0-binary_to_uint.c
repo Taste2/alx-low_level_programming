8ff31fa270df320638de68982889484bc790a423
Stephen Tawiah <stawiah17@gmail.com>
2022-10-28 04:43:45 -0700

    logic operator chabged fro OR to AND

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len, i, j, pow = 1;
+	int len, i, j;
+
+	int pow = 1;
 
 	unsigned int decima_l = 0;
+
+	if (!b)
+		return (0);
 
 	len = strlen(b);
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		if (b == NULL || b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
+
 		if (b[i] == '0')
 		{
 			j = 0;
